bce11f2508b121c4f9b151f46a3d1eb2d716db94
344_09 <344_09@DESKTOP-5QUJ579>
2025-06-06 14:08:02 +0700

    suc commit

--- src/appguessnumber/AppGuessNumber.java
+++ src/appguessnumber/AppGuessNumber.java
@@ -3,18 +3,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package appguessnumber;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author 344_09
  */
 public class AppGuessNumber {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        int numRandom ;
+        int numGuess;
+        int cont  = 0 ;
+        Random ran = new Random();
+        Scanner scan = new Scanner(System.in);
+        numRandom = ran.nextInt(3)+1;
+//        System.out.print("Number is : "+ numRandom);
+        while(cont<=3){
+            System.out.print("Enter yoour guess :");
+            numGuess = scan.nextInt();
+            if(numGuess==numRandom){
+                System.out.println("You win");
+                break;
+            }else if(numGuess>numRandom){
+                System.out.println("<<<< decrement");
+                cont++;
+            }else if(numGuess<numRandom){
+                System.out.println("Incrment >>>>>>");
+                cont++;
+            }
+            if(cont>=3){
+                System.out.println("you lose");
+            }
+            
+        }
+        
+    }     
 }
